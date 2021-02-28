8ad3083d10dec2efaf25ea6481007fd0d3b6a155
daniel <47526269+danielmartis@users.noreply.github.com>
2021-02-28 17:42:48 +0100

    Add files via upload

--- Practica1/AnalizadorLexico.java
+++ Practica1/AnalizadorLexico.java
@@ -8,6 +8,8 @@ class AnalizadorLexico {
     int bytes;
     RandomAccessFile archivo;
     Token t;
+    StringBuilder almacenado = new StringBuilder();
+
     public AnalizadorLexico(RandomAccessFile file){
         fil = 1;
         col = 1;
@@ -31,57 +33,119 @@ class AnalizadorLexico {
             return t;
         }
         leido = (char) b;
+        if(leido == ' ' || leido == '\t'){
+            col++;
+            siguienteToken();
+            estado = -1;
+        }
+        else if (leido == '\n'){
+            fil++;
+            col = 1;
+            siguienteToken();
+            estado = -1;
+        }
         //System.out.println(leido);
         switch(estado){
             case 1:
                 t.fila = fil;
                 t.columna = col;
-                fil++;
+                col++;
                 bytes++;
                 tok.append(leido);
                 if(leido == '(' ){
                     t.tipo = 0;
                     estado = 2;
                     t.lexema = tok.toString();
+                    break;
                 }
                 if(leido == ')'){
                     t.tipo = 1;
                     estado = 3;
                     t.lexema = tok.toString();
+                    break;
                 }
                 if(leido == ':'){
                     t.tipo = 2;
                     estado = 4;
                     t.lexema = tok.toString();
+                    break;
                 }
                 if(leido == '='){
                     t.tipo = 3;
                     estado = 5;
                     t.lexema = tok.toString();
+                    break;
                 }
                 if(leido == ';'){
                     t.tipo = 4;
                     estado = 6;
                     t.lexema = tok.toString();
+                    break;
                 }
                 if(leido == '-'){
                     estado = 7;
+                }
+                if(leido == 'f'){
+                    estado = 13;                    
                 }
                 else {
                     t.lexema = tok.toString();
                 }
             case 2:
-                break;
+                
             case 3:
-                break;
+                
             case 4:
-                break;
+                
             case 5:
-                break;
+    
             case 6:
-                break;
+                
             case 7:
                 
+            case 13:
+                if(estado == 13){
+
+                    try{
+                        b = archivo.read();
+                    }catch(IOException ie){
+                        System.out.println("Error");
+                    }
+                    leido = (char) b;
+                    col++;
+                    tok.append(leido);
+                    if(leido == 'n'){
+                        estado = 14;
+                    }
+                }
+            case 14:
+                if(estado == 14){
+
+                    try{
+                        b = archivo.read();
+                    }catch(IOException ie){
+                        System.out.println("Error");
+                    }
+                    col++;
+                    leido = (char) b;
+                    if((b >= 48 && b<= 57) || (b >= 65 && b<= 90) || (b >= 97 && b <= 122)){
+                        if(b != 98 && b!=105){
+                            estado = 16;
+                        }
+                    }
+                    else {
+                        t.tipo = 8;
+                        t.lexema = tok.toString();
+                        almacenado.append(leido);
+                    }
+                }
+            case 16:
+            /*
+                if(estado == 16){
+                    tok.append('p');
+                    t.lexema = tok.toString();
+                }
+            */
         }
         return t;
     }
--- Practica1/Token.java
+++ Practica1/Token.java
@@ -35,8 +35,27 @@ public class Token {
 		EOF		= 22;
 
 	public String toString(){
-	        // debes devolver para cada token la cadena de la 
-	        // tercera columna de la tabla que aparece en el enunciado
+	    switch(tipo){
+			case 0:
+				return "(";
+			case 1: 
+				return ")";
+			case 2:
+				return ":";
+			case 3:
+				return "->";
+			case 4:
+				return "=";
+			case 5:
+				return ";";
+			case 6:
+				return "+ -";
+			case 7:
+				return "* /";
+			case 8:
+				return "fn";
+		}
+		return "";
 	}
 }
 
